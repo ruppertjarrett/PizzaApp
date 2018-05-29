72ddb2e768b794b40c2dcb61d99a279e75c14017
Unknown <ruppertjarrett@yahoo.com>
2018-05-29 08:51:49 -0600

    Minor Changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("thebeast").addEventListener("click", thebeast);
 
 function meatlovers(evt) {
     console.log("running");
-    pizza.style.background = "url('Images/Toppings/PepperoniFull.png'), url('Images/Toppings/ItalianSausageFull.png'), url('Images/Toppings/BaconFull.png'), url('Images/Toppings/ChickenFull.png'), url('Images/Toppings/HamFull.png'), url('Images/Cheese/RegularCheese.png'), url('Images/Sauces/MarinaraSauce.png'), url('Images/Crust/Crust.png')";
+    pizza.style.background = "url('Images/Toppings/HamFull.png'), url('Images/Toppings/ItalianSausageFull.png'), url('Images/Toppings/BaconFull.png'), url('Images/Toppings/ChickenFull.png'), url('Images/Toppings/PepperoniFull.png'), url('Images/Cheese/RegularCheese.png'), url('Images/Sauces/MarinaraSauce.png'), url('Images/Crust/Crust.png')";
     pizza.style.backgroundRepeat = "no-repeat";
     pizza.style.backgroundSize = "cover";
 }
